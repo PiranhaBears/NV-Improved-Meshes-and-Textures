fede9dfc39233d0eb5a827bd2b1226132ad75759
PiranhaBears <piranhabears@gmail.com>
2016-06-11 23:47:15 -0400

    Update 1
    Whattap.

---
+++ Compile Mod.bat
@@ -0,0 +1,9 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+echo %mydate%_%mytime%
+
+"D:\Program Files\WinRAR\rar.exe" a -ep1 -r "Compiles\%mydate%_%mytime%" "E:\Projects\(B) Modding\Fallout\SMIM - FNV\(2) Full Mod\"
+
+echo "Mod compilation complete!"
+pause
